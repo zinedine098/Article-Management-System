34d664b5931ad0e531556c464a7f5fbcf2a9e079
zinedine098 <ibnuwawan80@gmail.com>
2026-02-08 11:55:01 +0700

    Add URL configurations and include apps for accounts, articles, comments, core, and pages

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'articles',
+    'comments',
+    'core',
+    'pages',
 ]
 
 MIDDLEWARE = [
